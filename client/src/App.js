04d1710614ab891d17759375a2dbf9f63d24c506
omar99 <omarshaikh@outlook.com>
2024-10-14 04:13:39 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -59,7 +59,7 @@ function App() {
     setButtonDisabled(true);
 
     try {
-      const response = await axios.post('https://7d56-2405-201-1f-2164-b3f0-fbe5-936d-1694.ngrok-free.app/scrape', { product });
+      const response = await axios.post('https://61e7-2405-201-1f-2164-b3f0-fbe5-936d-1694.ngrok-free.app/scrape', { product });
       setResults(response.data);
       
       // Scroll to results after loading
